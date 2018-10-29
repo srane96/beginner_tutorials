748bcc2084c57b2d90b712d39e65fde165a89a99
srane96 <srane96@terpmail.umd.edu>
2018-10-29 08:32:00 -0400

    Add doxygen comments to the source files

--- src/beginner_tutorials/src/listener.cpp
+++ src/beginner_tutorials/src/listener.cpp
@@ -1,14 +1,40 @@
+/**
+ *  @copyright (c) BSD
+ *  @file    listener.cpp
+ *  @author  Siddhesh Rane
+ *
+ *  @brief subscriber node;
+ *
+ *  @section DESCRIPTION
+ *
+ *  C++ program to implement subscriber node that subscribes to
+ *  the 'chatter_topic' and displays the message sent by publisher.
+ */
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-
+/**
+ * @brief A callback function to respond to messages
+ * @param msg string published by publisher node
+ * @return void
+ */
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-	ROS_INFO("Message Received: {%s}", msg->data.c_str());
+  ROS_INFO("Message Received: {%s}", msg->data.c_str());
 }
-
 int main(int argc, char **argv) {
-	ros::init(argc, argv, "subscriber");
-	ros::NodeHandle nh;
-	ros::Subscriber sub = nh.subscribe("chatter_topic", 1000, chatterCallback);
-	ros::spin();
-	return 0;
+  /// initialize the ros node
+  ros::init(argc, argv, "subscriber");
+  /// create an instance of NodeHandle
+  ros::NodeHandle nh;
+  /**
+   *  create a subscriber that subscribes to the
+   *  chatter topic with queue size of 1000
+   */
+  ros::Subscriber sub = nh.subscribe("chatter_topic", 1000,
+chatterCallback);
+  /*
+   * ask ROS to wait for and execute callbacks until
+   * the node shuts down.
+   */
+  ros::spin();
+  return 0;
 }
--- src/beginner_tutorials/src/talker.cpp
+++ src/beginner_tutorials/src/talker.cpp
@@ -1,22 +1,54 @@
+/**
+ *  @copyright (c) BSD
+ *  @file    talker.cpp
+ *  @author  Siddhesh Rane
+ *
+ *  @brief publisher node;
+ *
+ *  @section DESCRIPTION
+ *
+ *  C++ program to implement publisher node that publishes
+ *  custom string messages on 'chatter_topic'
+ */
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sstream>
 int main(int argc, char **argv) {
-	ros::init(argc, argv, "publisher");
-	ros::NodeHandle nh;
-	ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter_topic",1000);
-	ros::Rate loop_rate(10);
-	int count = 0;
-	while(ros::ok()) {
-		std_msgs::String msg;
-		std::stringstream ss;
-		ss << "Hello ENPM808X ROS: " << count;
-		msg.data = ss.str();
-		ROS_INFO("%s",msg.data.c_str());
-		chatter_pub.publish(msg);
-		ros::spinOnce();
-		loop_rate.sleep();
-		++count;	
-	}
-	return 0;
+  /// initialize the ros node
+  ros::init(argc, argv, "publisher");
+  /// create an instance of NodeHandle
+  ros::NodeHandle nh;
+  /**
+   *  create a publisher node that publishes standard
+   *  string messages to the chatter topic with queue
+   *  size of 1000
+   */
+  ros::Publisher chatter_pub = nh.advertise < std_msgs::String
+      > ("chatter_topic", 1000);
+  /// loop at 10Hz rate
+  ros::Rate loop_rate(10);
+  /// initiate the count
+  int count = 0;
+  /// loop until node is shutdown
+  while (ros::ok()) {
+    /// create String message
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss << "Hello ENPM808X ROS: " << count;
+    /// store custom string to message data
+    msg.data = ss.str();
+    /// display the message
+    ROS_INFO("%s", msg.data.c_str());
+    /// publish message using publisher object
+    chatter_pub.publish(msg);
+    /**
+     * ask ROS to execute all of the pending callbacks
+     * from all of the node’s subscriptions
+     */
+    ros::spinOnce();
+    /// wait until it's time for another iteration
+    loop_rate.sleep();
+    ++count;
+  }
+  return 0;
 }
