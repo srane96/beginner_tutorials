8ce1340a256dd6ce96084fb538791b0c21f3cdfb
srane96 <srane96@terpmail.umd.edu>
2018-10-30 13:22:37 -0400

    Update BSD license in talker.cpp and listener.cpp

--- src/beginner_tutorials/src/talker.cpp
+++ src/beginner_tutorials/src/talker.cpp
@@ -1,14 +1,37 @@
 /**
- *  @copyright (c) BSD
- *  @file    talker.cpp
- *  @author  Siddhesh Rane
+ * BSD 3-Clause License
+ * @copyright (c) 2018, Siddhesh Rane
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *  @brief publisher node;
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @file    talker.cpp
+ * @author  Siddhesh Rane
  *
- *  @section DESCRIPTION
+ * @brief publisher node;
  *
- *  C++ program to implement publisher node that publishes
- *  custom string messages on 'chatter_topic'
+ * @section DESCRIPTION
+ *
+ * C++ program to implement publisher node that publishes
+ * custom string messages on 'chatter_topic'
  */
 #include <ros/ros.h>
 #include <std_msgs/String.h>
